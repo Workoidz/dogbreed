2defabeccaf7bca7ef9909a329c049354faccc26
Ritesh Kapoor <ritesh@workoidz.com>
2024-05-15 13:33:01 +0530

    issue resolved

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -2,25 +2,37 @@ class BreedsController < ApplicationController
   require 'net/http'
 
   def index
+    # This action can be used to render a form for breed input or a list of breeds.
   end
 
   def fetch_breed
     breed = params[:breed].downcase
     url = URI("https://dog.ceo/api/breed/#{breed}/images/random")
-    response = Net::HTTP.get(url)
-    result = JSON.parse(response)
 
-    if result['status'] == 'success'
-      @breed_name = breed.capitalize
-      @image_url = result['message']
-    else
-      @error = "Breed not found"
+    begin
+      response = Net::HTTP.get(url)
+      result = JSON.parse(response)
+
+      if result['status'] == 'success'
+        @breed_name = breed.capitalize
+        @image_url = result['message']
+      else
+        @error = "Breed not found"
+      end
+    rescue JSON::ParserError => e
+      @error = "Error parsing the response"
+      Rails.logger.error("JSON::ParserError: #{e.message}")
+    rescue StandardError => e
+      @error = "An error occurred: #{e.message}"
+      Rails.logger.error("StandardError: #{e.message}")
     end
 
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_info', partial: 'breed_result', locals: { image_url: @image_url, breed_name: @breed_name }) }
-    #  format.turbo_stream
-    #  format.html { render partial: 'breed_result', status: :unprocessable_entity }
+      if @error
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_info', partial: 'breed_result', locals: { error: @error }) }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('breed_info', partial: 'breed_result', locals: { image_url: @image_url, breed_name: @breed_name, error: @error }) }
+      end
     end
   end
 end
